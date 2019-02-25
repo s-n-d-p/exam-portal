bdaaf584092515d501e00a4aefca091eb9698af3
gazlaws-dev <gazlaws.dev@gmail.com>
2019-02-25 14:22:02 +0530

    Fixes #27

--- static/php/registrationPage.php
+++ static/php/registrationPage.php
@@ -9,13 +9,11 @@
     $hashed_password = password_hash($password,PASSWORD_DEFAULT);
     $cgpa = mysqli_real_escape_string($conn,$_POST['cgpa']);
     $dob = mysqli_real_escape_string($conn,$_POST['dob']);
-    $parsed_dob = '1997-06-05';
-    //TODO
     $course = mysqli_real_escape_string($conn,$_POST['course']);
     
-    $insert_query = "insert into Student(first_name, last_name, email_id, password, dob, course, cgpa) values('{$first_name}','${last_name}', '${email_id}', '${hashed_password}','${parsed_dob}','${course}', '${cgpa}')";
+    $insert_query = "insert into Student(first_name, last_name, email_id, password, dob, course, cgpa) values('{$first_name}','${last_name}', '${email_id}', '${hashed_password}','${dob}','${course}', '${cgpa}')";
     
-    // echo $insert_query;
+    //echo $insert_query;
     
     session_start();
     if(mysqli_query($conn,$insert_query)){
